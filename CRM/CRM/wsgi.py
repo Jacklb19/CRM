93bde64610fc04dbe4553263ee1084f0d7c26124
Davhumpf <vincho0528@gmail.com>
2025-11-20 09:02:37 -0500

    wsgi

--- CRM/CRM/wsgi.py
+++ CRM/CRM/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Asegura que Django use tu archivo de settings correcto
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRM.settings')
 
+# Render necesita esta aplicación WSGI para levantar el servidor con Gunicorn
 application = get_wsgi_application()
